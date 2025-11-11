43d331b55cb8a1b4a18f83077e420f5155b0d755
Mariem Guermazi <mariem.guermazi01@gmail.com>
2025-11-11 12:02:00 +0100

    Initial commit

---
+++ src/main/java/tn/pi/gestiondescomptesbancaires/GestionDesComptesBancairesApplication.java
@@ -0,0 +1,13 @@
+package tn.pi.gestiondescomptesbancaires;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDesComptesBancairesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionDesComptesBancairesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/pi/gestiondescomptesbancaires/GestionDesComptesBancairesApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.pi.gestiondescomptesbancaires;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDesComptesBancairesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
